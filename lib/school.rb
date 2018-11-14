514e415c15b97baede7a57caa2a0193fbe41a5ca
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-14 15:22:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  attr_accessor :roster, :student_name, :school_name
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+  if @roster[grade]
+    @roster[grade] << name
+  else
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+  end
+
+
+end
