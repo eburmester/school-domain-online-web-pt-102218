8212553db256bdf6f77f596063b6c65754ef641f
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-14 15:23:36 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,6 +14,15 @@ class School
     @roster[grade] << name
   end
   end
+  
+  def grade(number)
+    @roster[number]
+  end
 
-
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+  
 end
